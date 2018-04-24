78466694c14da39d6952577926460df3abdc0c8f
Digvijay <digv22@gmail.com>
2018-04-24 20:25:19 +0530

    added unique random

--- quiz_func.c
+++ quiz_func.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
-#include<math.h>
-#include<stdlib.h>
-#include<ctype.h>
-#include<string.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <time.h>
@@ -11,12 +11,16 @@
 #define buffSIZE 20000
 #define ansSIZE 50
 
+int breakflag = 1;
+void handle();
+
 void quizType(int qType)
 {
 
 	char buff[buffSIZE];
 	questionType q[ansSIZE]; //to store questions
 	//int fEasy,fMed,fHard;
+	int randomQuestionId[10];
 	int f1, num = 0;
 	int lower = 0, upper = 0;
 	int flag = 0;
@@ -40,17 +44,17 @@ void quizType(int qType)
 	}
 	if (f1 < 0)
 	{
-		
+
 		perror("error");
 		exit(1);
 	}
-	
-	size=read(f1, buff, sizeof(buff));
-      	printf("the file's length is %d\n",size);
-	for(i=0;i<size;i++)
+
+	size = read(f1, buff, sizeof(buff));
+	printf("the file's length is %d\n", size);
+	for (i = 0; i < size; i++)
 	{
-		char *quest=malloc(500 * sizeof(char));
-		if(buff[i]=='*')
+		char *quest = malloc(500 * sizeof(char));
+		if (buff[i] == '*')
 		{
 			j = 0;
 			while (buff[i + 1] != '%')
@@ -78,8 +82,8 @@ void quizType(int qType)
 			q[k].corrAns = ans;
 			strcpy(q[k].fullQues, quest);
 			free(quest);
-		//	printf("%s\n",q[k].fullQues);
-		//	printf("%c\n\n",q[k].corrAns);
+			//	printf("%s\n",q[k].fullQues);
+			//	printf("%c\n\n",q[k].corrAns);
 			k++;
 			//	printf("\n\n");
 		}
@@ -89,18 +93,50 @@ void quizType(int qType)
 	printf("\n\t******--->N0TE +4 for corect answer and -1 for incorrect******\n\n");
 	int ran = 0;
 	range = (k + 1) / 10;
-	upper = range - 1;
+	upper = k; //range - 1;
 	printf("\nk:%d, upper %d, range %d", k, upper, range);
-	for (int i = 0; i < 10; i++)
+
+	for (i = 0; i < 10; i++)
 	{
-		srand(time(NULL));
+
+	regen:
+		//srand(time(NULL));
 		ran = rand();
 		num = (rand() % (upper - lower + 1)) + lower;
+
+		randomQuestionId[i] = num;
+		for (int j = 0; j < 10; j++)
+		{
+			if (randomQuestionId[i] == randomQuestionId[j])
+			{
+				if(randomQuestionId[i]+1 < upper ){
+					randomQuestionId[i]= randomQuestionId[i]+ 1;
+				}else
+					goto regen;
+			}
+		}
+	}
+
+	printf("\nRandom question numbers :\n");
+	for (i = 0; i < 10; i++)
+	{
+		printf("\n%d",randomQuestionId[i]);
+	
+	}
+
+	signal(SIGALRM, handle);
+		
+	i = 0;
+time_exhausted:
+	for (; i < 10 && breakflag; i++)
+	{
+
 		printf("time:%d\n\n", ran);
 		printf("num %d\n\n", num);
 		lower = lower + range;
 		upper = upper + range;
-		printf("%d)%s", i + 1, q[num].fullQues);
+		printf("%d)%s", i + 1, q[randomQuestionId[i]].fullQues);
+		alarm(2);
 		printf("enter your answer\n");
 		getchar();
 		scanf("%c", &ans);
@@ -116,5 +152,17 @@ void quizType(int qType)
 			printf("\n******INCORRECT, The right answer is '%c'******\n\n", q[num].corrAns);
 		}
 	}
+	if (i != 9)
+	{
+		printf("\n Time up\n\n");
+		breakflag = 1;
+		goto time_exhausted;
+	}
+
 	printf("\nYour final score is %d (out of 40)\n", total);
+}
+
+void handle()
+{
+	breakflag = 0;
 }
